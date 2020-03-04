00e12b485c255fa30fd0a2f02ef9d4d2df115e7f
Amnah Abdulal <as-1991-as@hotmail.com>
2020-03-04 11:34:03 +0300

    adding map

--- js/main.js
+++ js/main.js
@@ -12,6 +12,17 @@ document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
   fetchCuisines();
 });
+ 
+initMap = () =>{
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYW1uYWgxOTkxIiwiYSI6ImNrN2E2bnF5bTAwMHEzZHNid296Y3NxdDQifQ.WTP_xHzNp71kwdlnGUr_pA';
+  var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+  });
+}
+
+
+
 
 /**
  * Fetch all neighborhoods and set their HTML.
@@ -78,7 +89,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYW1uYWgxOTkxIiwiYSI6ImNrN2E2bnF5bTAwMHEzZHNid296Y3NxdDQifQ.WTP_xHzNp71kwdlnGUr_pA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +

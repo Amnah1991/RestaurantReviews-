059d84d66f500405d5aa52b6e23e160a0682b10d
Amnah Abdulal <as-1991-as@hotmail.com>
2020-03-09 21:46:33 +0300

    css + readme

--- js/main.js
+++ js/main.js
@@ -171,6 +171,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = `${restaurant} restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
